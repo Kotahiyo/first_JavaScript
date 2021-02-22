ce34b009025d936da276c8e5bdca32d609f3a8b1
Kouhei <hiyotihakou@gmail.com>
2021-02-22 10:49:52 +0900

    関数の基本構文と実行のトリガーを学習

--- javascript/03/javascript/main.js
+++ javascript/03/javascript/main.js
@@ -6,3 +6,14 @@ const number = 30;
 const result = plus1(number);
 
 console.log(result);
+
+// これだけだと実行されない
+function alertMessage() {
+  const text = 'テキスト';
+  alert(text);
+}
+
+// 以下が必要
+// alertMessage();
+// () で関数の実行をするため、必ずつける
+// alertMessage;
