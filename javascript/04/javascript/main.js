ef0971c567f0226cd0757c847d4046ac40add707
Kouhei <hiyotihakou@gmail.com>
2021-02-23 08:55:16 +0900

    DOM の操作と作成、追加

--- javascript/04/javascript/main.js
+++ javascript/04/javascript/main.js
@@ -4,7 +4,17 @@
 // window.alert('tewst');
 
 // DOM の取得
-const elements = document.getElementsByClassName('foo');
-console.log(elements);
-const elements = document.getElementById('bar');
-console.log(elements);
+// const element = document.getElementsByClassName('foo');
+// console.log(element);
+const element = document.getElementById('bar');
+console.log(element);
+
+// DOM の操作
+element.innerText = 'testだよ';
+
+// 新しくPタグを作成し、変数に格納
+const newLine = document.createElement('p');
+// Pタグに新しくコメントを追加
+newLine.innerText = '新しい行です';
+// どこに出力するか指定
+element.appendChild(newLine);
