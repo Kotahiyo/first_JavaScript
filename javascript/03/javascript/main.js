aed15818a7b57050ad5aa87f15a1ee0a50d48f3d
Kouhei <hiyotihakou@gmail.com>
2021-02-22 11:02:58 +0900

    関数を変数に代入した結果と表示の違いについて学習

--- javascript/03/javascript/main.js
+++ javascript/03/javascript/main.js
@@ -1,17 +1,29 @@
-function plus1(number) {
+// 関数の代入。関数の名前をつけないことを無名関数と呼ぶ
+const plus1 = function (number) {
   const result = number + 1;
   return result;
-}
-const number = 30;
-const result = plus1(number);
+};
 
-console.log(result);
+// 関数を変数に代入しただけ
+const plus1Func = plus1;
+// 関数の処理の結果を代入
+const plus1Result = plus1(300);
 
-// これだけだと実行されない
-function alertMessage() {
-  const text = 'テキスト';
-  alert(text);
-}
+// 関数のみがログに出力
+console.log(plus1Func);
+// 関数結果がログに出力
+console.log(plus1Result);
+
+// const number = 30;
+// const result = plus1(number);
+
+// console.log(result);
+
+// // これだけだと実行されない
+// function alertMessage() {
+//   const text = 'テキスト';
+//   alert(text);
+// }
 
 // 以下が必要
 // alertMessage();
