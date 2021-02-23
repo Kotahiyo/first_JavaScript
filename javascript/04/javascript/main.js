b37d7d9d8bf5bd551b2a1868178eb2e0f116333a
Kouhei <hiyotihakou@gmail.com>
2021-02-23 09:07:43 +0900

    EventListener について

--- javascript/04/javascript/main.js
+++ javascript/04/javascript/main.js
@@ -10,11 +10,26 @@ const element = document.getElementById('bar');
 console.log(element);
 
 // DOM の操作
-element.innerText = 'testだよ';
+// element.innerText = 'testだよ';
 
 // 新しくPタグを作成し、変数に格納
-const newLine = document.createElement('p');
+// const newLine = document.createElement('p');
 // Pタグに新しくコメントを追加
-newLine.innerText = '新しい行です';
+// newLine.innerText = '新しい行です';
 // どこに出力するか指定
-element.appendChild(newLine);
+// element.appendChild(newLine);
+
+// 変数に格納
+const clickAlert = function () {
+  alert('アラートだよ！');
+  console.log('押したら出力される');
+};
+
+const clickComment = function () {
+  const newComment = document.createElement('div');
+  newComment.innerText = '押したら出力される';
+  element.appendChild(newComment);
+};
+
+// EventListener
+element.addEventListener('click', clickComment);
