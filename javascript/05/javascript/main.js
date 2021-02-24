0fdf8bdca95f88375785c62035ea50f7b2002f22
Kouhei <hiyotihakou@gmail.com>
2021-02-24 09:21:38 +0900

    キーワード引数

--- javascript/05/javascript/main.js
+++ javascript/05/javascript/main.js
@@ -1,11 +1,19 @@
-// デフォルト引数
-function execute(arg = 'test') {
-  return arg;
+// // デフォルト引数
+// function execute(arg = 'test') {
+//   return arg;
+// }
+
+// // 関数
+// const result1 = execute();
+// console.log(result1);
+
+// const result2 = execute('foo');
+// console.log(result2);
+
+// キーワード引数
+function execute({ arg1, arg2 }) {
+  return { arg1, arg2 };
 }
 
-// 関数
-const result1 = execute();
-console.log(result1);
-
-const result2 = execute('foo');
-console.log(result2);
+const result = execute({ arg1: 'test1', arg2: 'test2' });
+console.log(result);
