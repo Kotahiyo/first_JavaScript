b6e44ceabacb247968a1d963b57a39ab4b2e113f
Kouhei <hiyotihakou@gmail.com>
2021-02-24 10:04:28 +0900

    真偽判定

--- javascript/06/javascript/main.js
+++ javascript/06/javascript/main.js
@@ -1,10 +1,19 @@
-// if 文の基礎
-const number = 11;
+// // if 文の基礎
+// const number = 11;
 
-if (number > 10) {
-  console.log('10 より大きいよ');
-} else if (number === 10) {
-  console.log('10 だよ');
+// if (number > 10) {
+//   console.log('10 より大きいよ');
+// } else if (number === 10) {
+//   console.log('10 だよ');
+// } else {
+//   console.log('10 未満だよ');
+// }
+
+// 真偽判定
+const val = null;
+// JavaScript で偽になるのは false, NaN, null, 0, undefined
+if (val) {
+  console.log('真だよ');
 } else {
-  console.log('10 未満だよ');
+  console.log('偽だよ');
 }
