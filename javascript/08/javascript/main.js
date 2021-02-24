a92a6264041f4dd2c7af1701fc947bb8ad519e14
Kouhei <hiyotihakou@gmail.com>
2021-02-25 08:56:59 +0900

    アロー関数について

---
+++ javascript/08/javascript/main.js
@@ -0,0 +1,12 @@
+// 繰り返し処理
+const numbers = [1, 2, 3];
+
+// numbers.forEach(function (num) {
+//   console.log(num * 2);
+// });
+
+// アロー関数
+numbers.forEach((num) => {
+  console.log(num * 2);
+});
+// 無名関数の時に上記の記述ができることに注意
