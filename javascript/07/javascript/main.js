d448d4b2cd84c37d22ae675111f24bf252570668
Kouhei <hiyotihakou@gmail.com>
2021-02-24 11:09:38 +0900

    スプレット演算子の特徴や使い方

--- javascript/07/javascript/main.js
+++ javascript/07/javascript/main.js
@@ -22,9 +22,22 @@
 // console.log(numbers);
 
 // スプレット演算子
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
 // NaN と出力
-console.log(Math.max(numbers));
+// console.log(Math.max(numbers));
 // 正しく出力される
-console.log(Math.max(...numbers));
+// console.log(Math.max(...numbers));
+
+// スプレット演算子は配列を繋げることが出来る
+const numbers1 = [1, 2, 3];
+const numbers2 = [20, 30, 40];
+
+// 出力結果    1,2,320,30,40
+console.log(numbers1 + numbers2);
+
+// 出力結果    (2) [Array(3), Array(3)]0: (3) [1, 2, 3]1: (3) [20, 30, 40]length: 2__proto__: Array(0)
+console.log([numbers1, numbers2]);
+
+// 出力結果    [1, 2, 3, 20, 30, 40]
+console.log([...numbers1, ...numbers2]);
