940dc391d1832ba216c52f986f69a375b6229c94
Kouhei <hiyotihakou@gmail.com>
2021-02-25 09:30:17 +0900

    map の使い方

--- javascript/08/javascript/main.js
+++ javascript/08/javascript/main.js
@@ -1,12 +1,20 @@
 // 繰り返し処理
 const numbers = [1, 2, 3];
 
-// numbers.forEach(function (num) {
+// // numbers.forEach(function (num) {
+// //   console.log(num * 2);
+// // });
+
+// // アロー関数
+// numbers.forEach((num) => {
 //   console.log(num * 2);
 // });
+// // 無名関数の時に上記の記述ができることに注意
 
-// アロー関数
-numbers.forEach((num) => {
-  console.log(num * 2);
+// map の使い方
+const result = numbers.map((num) => {
+  // 戻り値を指定してあげることに注意
+  return num * 2;
 });
-// 無名関数の時に上記の記述ができることに注意
+
+console.log(result);
