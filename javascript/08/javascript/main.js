b54e85479f478af027ddb9f18917ed37821d3dd2
Kouhei <hiyotihakou@gmail.com>
2021-02-25 09:43:51 +0900

    オブジェクトの繰り返し文

--- javascript/08/javascript/main.js
+++ javascript/08/javascript/main.js
@@ -1,5 +1,5 @@
 // 繰り返し処理
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
 // // numbers.forEach(function (num) {
 // //   console.log(num * 2);
@@ -12,9 +12,26 @@ const numbers = [1, 2, 3];
 // // 無名関数の時に上記の記述ができることに注意
 
 // map の使い方
-const result = numbers.map((num) => {
-  // 戻り値を指定してあげることに注意
-  return num * 2;
-});
+// const result = numbers.map((num) => {
+//   戻り値を指定してあげることに注意
+//   return num * 2;
+// });
 
-console.log(result);
+// console.log(result);
+
+// オブジェクトの繰り返し文
+const user = {
+  name: 'fujii',
+  age: 29,
+};
+// オブジェクトのキーを取り出している
+const keys = Object.keys(user);
+
+keys.forEach((key) => {
+  console.log(key);
+  // key と value を持っているもの = オブジェクト
+  // オブジェクト[key] と指定すると value が取り出せる
+  // 以下では user["name"], user["age"] と同じ意味
+  const value = user[key];
+  console.log(value);
+});
