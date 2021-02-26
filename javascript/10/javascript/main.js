e69f82399ba658f887ad20c59a5e99e60f4bfaa2
Kouhei <hiyotihakou@gmail.com>
2021-02-26 17:04:57 +0900

    constructor とゲッター、セッター

--- javascript/10/javascript/main.js
+++ javascript/10/javascript/main.js
@@ -1,11 +1,27 @@
 // クラスの宣言
+// class Robot {
+//   // クラス内ではアローや function 表記が使えない
+//   sayHello() {
+//     console.log('Hello');
+//   }
+// }
+// // インスタンスの生成
+// const robot = new Robot();
+// // 実行
+// robot.sayHello();
+
+// クラスの初期値設定
 class Robot {
-  // クラス内ではアローや function 表記が使えない
-  sayHello() {
-    console.log('Hello');
+  constructor(color) {
+    this.color = color;
   }
 }
-// インスタンスの生成
-const robot = new Robot();
-// 実行
-robot.sayHello();
+
+const robot = new Robot('red');
+
+// ゲッター
+console.log(robot.color);
+// セッター
+robot.color = 'blue';
+
+console.log(robot.color);
